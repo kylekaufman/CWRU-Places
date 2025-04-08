5a18a63153893ad76d00286714b38edcc020b2ed
kylekaufman <ktk33@case.edu>
2025-04-08 11:16:30 -0400

    Initial Commit

---
+++ CWRU Places/CWRU_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWRU_PlacesApp.swift
+//  CWRU Places
+//
+//  Created by Kyle Kaufman on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CWRU_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
